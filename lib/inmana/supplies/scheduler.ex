fcb2027174c1c099ef021561c8152ba0ae558f14
Elrond <alberto.g.barrios@outlook.com>
2021-04-22 11:05:09 -0300

    iniciar servidor e ja iniciar uma tarefa

--- lib/inmana/supplies/scheduler.ex
+++ lib/inmana/supplies/scheduler.ex
@@ -2,11 +2,19 @@ defmodule Inmana.Supplies.Scheduler do
   use GenServer
   alias Inmana.Supplies.ExpirationNotification
 
+  # Cliente --------------------------------------
+  def start_link(_state) do
+    GenServer.start_link(__MODULE__, %{})
+  end
+
+  # Servidor --------------------------------------
+  @impl true
   def init(state \\ %{}) do
     schedule_notification()
     {:ok, state}
   end
 
+  @impl true
   def handle_info(:generate, state) do
     ExpirationNotification.send()
 
