92095a3b308961cd554b363980c3618b96fd4c8b
Elrond <alberto.g.barrios@outlook.com>
2021-04-22 10:51:58 -0300

    agendamento do email

--- lib/inmana/supplies/scheduler.ex
+++ lib/inmana/supplies/scheduler.ex
@@ -1,17 +1,21 @@
 defmodule Inmana.Supplies.Scheduler do
   use GenServer
+  alias Inmana.Supplies.ExpirationNotification
 
   def init(state \\ %{}) do
+    schedule_notification()
     {:ok, state}
   end
 
-  # essa função é assíncrona
-  def handle_cast({:put, key, value}, state) do
-    {:noreply, Map.put(state, key, value)}
+  def handle_info(:generate, state) do
+    ExpirationNotification.send()
+
+    schedule_notification()
+
+    {:noreply, state}
   end
 
-  # essa função é sincrona
-  def handle_call({:get, key}, _from, state) do
-    {:reply, Map.get(state, key), state}
+  def schedule_notification do
+    Process.send_after(self(), :generate, 1000 * 10)
   end
 end
