8859536fc27ea3639357fc6c25896b08c8dc2b47
Elrond <alberto.g.barrios@outlook.com>
2021-04-23 17:32:58 -0300

    testes de controllers

---
+++ test/inmana_web/controllers/restaurants_controller_test.exs
@@ -0,0 +1,37 @@
+defmodule InmanaWeb.RestaurantsControllerTest do
+  use InmanaWeb.ConnCase
+
+  # describe create/2 o barra 2 é de dois parametros
+  describe "create/2" do
+    test "When all params are valid, creates the user", %{conn: conn} do
+      params = %{name: "Siri cascudo", email: "siri@cascudo.com"}
+
+      response =
+        conn
+        |> post(Routes.restaurants_path(conn, :create, params))
+        |> json_response(:created)
+
+      assert %{
+               "message" => "Restaurant created!",
+               "restaurant" => %{
+                 "email" => "siri@cascudo.com",
+                 "id" => _id,
+                 "name" => "Siri cascudo"
+               }
+             } = response
+    end
+
+    test "When there are invalid params, returns an error", %{conn: conn} do
+      params = %{email: "siri@cascudo.com"}
+
+      expeted_response = %{"message" => %{"name" => ["can't be blank"]}}
+
+      response =
+        conn
+        |> post(Routes.restaurants_path(conn, :create, params))
+        |> json_response(:bad_request)
+
+      assert response == expeted_response
+    end
+  end
+end
